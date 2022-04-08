a68388017a8bcaed0f773d0c8fdf4a70be0a05c6
iammiori <immioriii@gmail.com>
2022-04-09 01:47:46 +0900

    [feat] init the project

---
+++ CrossfitSTBJamsil/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CrossfitSTBJamsil
+//
+//  Created by miori Lee on 2022/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
